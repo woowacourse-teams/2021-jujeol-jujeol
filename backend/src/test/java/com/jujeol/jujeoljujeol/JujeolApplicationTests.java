f3226b72f5dd8c914795dc51082719ec5f1f8bc9
sihyung92 <fjzjqhdl@gmail.com>
2021-07-06 20:39:54 +0900

    jujeol-app

---
+++ backend/src/test/java/com/jujeol/jujeoljujeol/JujeolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jujeol.jujeoljujeol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JujeolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
