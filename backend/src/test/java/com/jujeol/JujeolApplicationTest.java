e98fdd87126db3b6f6cde4cf3ff4b589d7b27fd6
sihyung92 <fjzjqhdl@gmail.com>
2021-07-07 15:20:10 +0900

    feat: bootApplication 생성 및 테스트

---
+++ backend/src/test/java/com/jujeol/JujeolApplicationTest.java
@@ -0,0 +1,13 @@
+package com.jujeol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JujeolApplicationTest {
+
+  @Test
+  void contextLoads() {
+
+  }
+}
