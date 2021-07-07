ad152625bcb62ece61f9f44938557977a4a4022a
sunhpark42 <sunhpark42@gmail.com>
2021-07-07 22:38:11 +0900

    feat: 카드 컴포넌트 구현

---
+++ frontend/src/components/Card/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './Card.styles';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Card = ({ children }: Props) => {
+  return <Container>{children}</Container>;
+};
+
+export default Card;
