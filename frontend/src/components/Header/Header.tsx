2c6a3a522bcbec31e193671c0ef70b07ecdebdcd
sunhpark42 <sunhpark42@gmail.com>
2021-07-08 14:22:32 +0900

    feat: Header 구현

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './Header.styles';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Header = ({ children }: Props) => {
+  return <Container>{children}</Container>;
+};
+
+export default Header;
