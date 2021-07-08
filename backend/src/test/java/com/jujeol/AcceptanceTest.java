e9781fec41b465453d73fa666cc79cd10f27e1cc
perenok <perenoke@gmail.com>
2021-07-08 14:04:14 +0900

    feat: RestDocs 초기 설정
    - AcceptanceTest 에 문서화 설정 추가

--- backend/src/test/java/com/jujeol/AcceptanceTest.java
+++ backend/src/test/java/com/jujeol/AcceptanceTest.java
@@ -1,28 +1,40 @@
 package com.jujeol;
 
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ExtendWith(RestDocumentationExtension.class)
 @ActiveProfiles("test")
 public class AcceptanceTest {
 
     @LocalServerPort
-    int port;
-    boolean logFlag;
+    private int port;
+    private boolean logFlag;
+    private RequestSpecification spec;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp(RestDocumentationContextProvider restDocumentation) {
         RestAssured.port = port;
         logFlag = true;
+        spec = new RequestSpecBuilder()
+                .addFilter(documentationConfiguration(restDocumentation))
+                .build();
     }
 
     public void showLog(boolean logFlag) {
