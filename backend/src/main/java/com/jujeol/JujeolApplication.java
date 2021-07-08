44fe67674652286596b743ad4b9892beb06ebacc
SungSiHyung <51393021+sihyung92@users.noreply.github.com>
2021-07-08 10:57:56 +0900

    chore: 백엔드 탭 정렬 수정 (#13)

--- backend/src/main/java/com/jujeol/JujeolApplication.java
+++ backend/src/main/java/com/jujeol/JujeolApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JujeolApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(JujeolApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(JujeolApplication.class, args);
+    }
 }
