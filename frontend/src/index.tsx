f5eef65e33082c07a0c523b7b409a074fa0ca2c5
sunhpark42 <sunhpark42@gmail.com>
2021-07-08 21:43:07 +0900

    feat: ReactQueryClientProvider 구현

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import APIProvider from './apis/APIProvider';
 import App from './App';
 import GlobalStyle from './GlobalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <APIProvider>
+      <GlobalStyle />
+      <App />
+    </APIProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
