30bd65ee56286751aad78e52ae6cf08223c9a33d
sihyung92 <fjzjqhdl@gmail.com>
2021-07-10 11:25:48 +0900

    feat: 예외 공통 코드 작성
    - 예외 코드와 내용이 정의된 enum 생성
    - 예외 코드를 공통으로 hanlding 할 Controller Advice 생성
    - 404 예외에 대한 핸들링

---
+++ backend/src/main/java/com/jujeol/commons/exception/ExceptionController.java
@@ -0,0 +1,26 @@
+package com.jujeol.commons.exception;
+
+import static com.jujeol.commons.exception.ExceptionCodeAndDetails.NOT_FOUND_API;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@RestControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(JujeolException.class)
+    public ResponseEntity<JujeolExceptionDto> loginExceptionHandler(JujeolException exception) {
+        return ResponseEntity.badRequest().body(new JujeolExceptionDto(exception.getCode(), exception.getMessage()));
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<JujeolExceptionDto> handleError404()   {
+        String code = NOT_FOUND_API.getCode();
+        String message = NOT_FOUND_API.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new JujeolExceptionDto(code, message));
+    }
+}
