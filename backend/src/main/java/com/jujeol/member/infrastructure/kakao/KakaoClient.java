f1bb3442dac980351e10f48c3d279cf5ec0c1f68
bomin <bomin_93@naver.com>
2021-07-11 20:18:01 +0900

    refactor: kakao oauth 정보 관리 객체 생성

--- backend/src/main/java/com/jujeol/member/infrastructure/kakao/KakaoClient.java
+++ backend/src/main/java/com/jujeol/member/infrastructure/kakao/KakaoClient.java
@@ -5,7 +5,6 @@ import com.jujeol.member.application.SocialClient;
 import com.jujeol.member.infrastructure.ClientResponseConverter;
 import com.jujeol.member.infrastructure.kakao.dto.KakaoAccessTokenRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -25,19 +24,7 @@ public class KakaoClient implements SocialClient {
 
     private final RestTemplate restTemplate;
     private final ClientResponseConverter clientResponseConverter;
-
-    @Value("${kakao.client.id:123}")
-    private String clientId;
-    @Value("${kakao.client.secret:123}")
-    private String clientSecret;
-    @Value("${kakao.url.access-token:/default}")
-    private String kakaoTokenUrl;
-    @Value("${kakao.url.profile:/default}")
-    private String kakaoUserUrl;
-    @Value("${kakao.client.redirect-url:/default}")
-    private String kakaoRedirectUrl;
-    @Value("${kakao.url.unlink:/default}")
-    private String kakaoUnlinkUrl;
+    private final KakaoOauthInfo kakaoOauthInfo;
 
     @Override
     public String getAccessToken(String code) {
@@ -46,10 +33,11 @@ public class KakaoClient implements SocialClient {
         headers.add(CONTENT_TYPE, DEFAULT_CHARSET);
 
         final KakaoAccessTokenRequest kakaoAccessTokenRequest = new KakaoAccessTokenRequest(
-                GRANT_TYPE, clientId, kakaoRedirectUrl, code, clientSecret);
+                GRANT_TYPE, kakaoOauthInfo.getClientId(), kakaoOauthInfo.getKakaoRedirectUrl(), code,
+                kakaoOauthInfo.getClientSecret());
 
         ResponseEntity<String> response = restTemplate.exchange(
-                kakaoTokenUrl,
+                kakaoOauthInfo.getKakaoTokenUrl(),
                 HttpMethod.POST,
                 new HttpEntity<>(
                         clientResponseConverter.convertHttpBody(kakaoAccessTokenRequest),
@@ -65,7 +53,7 @@ public class KakaoClient implements SocialClient {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(AUTHORIZATION, String.format(BEARER_FORM, accessToken));
         final String body = restTemplate
-                .exchange(kakaoUserUrl, HttpMethod.GET, new HttpEntity<>(httpHeaders),
+                .exchange(kakaoOauthInfo.getKakaoUserUrl(), HttpMethod.GET, new HttpEntity<>(httpHeaders),
                         String.class)
                 .getBody();
 
@@ -77,6 +65,6 @@ public class KakaoClient implements SocialClient {
     public void unlink(String accessToken) {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(AUTHORIZATION, String.format(BEARER_FORM, accessToken));
-        restTemplate.exchange(kakaoUnlinkUrl, HttpMethod.GET, new HttpEntity<>(httpHeaders), Void.class);
+        restTemplate.exchange(kakaoOauthInfo.getKakaoUnlinkUrl(), HttpMethod.GET, new HttpEntity<>(httpHeaders), Void.class);
     }
 }
