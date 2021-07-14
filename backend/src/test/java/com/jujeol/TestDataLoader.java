2df0e5dd2341d16354d27dc8522f9ba4721cb04d
pika96 <yjwdbswldn@naver.com>
2021-07-15 01:56:43 +0900

    feat: TestDataLoader 리뷰 데이터 추가

--- backend/src/test/java/com/jujeol/TestDataLoader.java
+++ backend/src/test/java/com/jujeol/TestDataLoader.java
@@ -30,21 +30,24 @@ public class TestDataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Drink stella = Drink.from(
-                "스텔라", "stella",5.5, "KakaoTalk_Image_2021-07-08-19-58-09_001.png", Category.BEER);
+                "스텔라", "stella", 5.5, "KakaoTalk_Image_2021-07-08-19-58-09_001.png", Category.BEER);
         Drink kgb = Drink.from(
-                "KGB", null,3.5, "KakaoTalk_Image_2021-07-08-19-58-09_002.png", Category.BEER);
+                "KGB", null, 3.5, "KakaoTalk_Image_2021-07-08-19-58-09_002.png", Category.BEER);
         Drink estp = Drink.from(
                 "ESTP", null, 7.5, "KakaoTalk_Image_2021-07-08-19-58-11_003.png", Category.BEER);
         Drink tiger_rad = Drink.from(
-                "타이거 라들러 자몽", "Tiger_Rad", 9.5, "KakaoTalk_Image_2021-07-08-19-58-15_004.png", Category.BEER);
+                "타이거 라들러 자몽", "Tiger_Rad", 9.5, "KakaoTalk_Image_2021-07-08-19-58-15_004.png",
+                Category.BEER);
         Drink tsingtao = Drink.from(
-                "칭따오", "TSINGTAO", 12.0, "KakaoTalk_Image_2021-07-08-19-58-18_005.png", Category.BEER);
+                "칭따오", "TSINGTAO", 12.0, "KakaoTalk_Image_2021-07-08-19-58-18_005.png",
+                Category.BEER);
         Drink apple = Drink.from(
                 "애플", "Apple", 8.2, "KakaoTalk_Image_2021-07-08-19-58-20_006.png", Category.BEER);
         Drink ob = Drink.from(
                 "오비", "OB", 85.0, "KakaoTalk_Image_2021-07-08-19-58-22_007.png", Category.BEER);
         Drink tigerLemon = Drink.from(
-                "타이거 라들러 레몬", "Tiger_Lemon", 4.5, "KakaoTalk_Image_2021-07-08-19-58-22_008.png", Category.BEER);
+                "타이거 라들러 레몬", "Tiger_Lemon", 4.5, "KakaoTalk_Image_2021-07-08-19-58-22_008.png",
+                Category.BEER);
 
         List<Drink> beers = Arrays.asList(
                 stella, kgb, estp, tiger_rad, tsingtao, apple, ob, tigerLemon);
@@ -54,9 +57,21 @@ public class TestDataLoader implements CommandLineRunner {
         Review review1 = Review.from("천재 윤피카", stella);
         Review review2 = Review.from("천재 크로플", stella);
         Review review3 = Review.from("천재 나봄", stella);
-        Review review4 = Review.from("천재 웨지", stella);
+        Review review4 = Review.from("천재 소롱", stella);
+        Review review5 = Review.from("천재 소롱", stella);
+        Review review6 = Review.from("천재 소롱", stella);
+        Review review7 = Review.from("천재 소롱", stella);
+        Review review8 = Review.from("천재 소롱", stella);
+        Review review9 = Review.from("천재 소롱", stella);
+        Review review10 = Review.from("바보 피카", stella);
+        Review review11 = Review.from("바보 피카", stella);
+        Review review12 = Review.from("바보 피카", stella);
 
-        List<Review> reviews = List.of(review1, review2, review3, review4);
+        List<Review> reviews = List.of(
+                review1, review2, review3, review4,
+                review5, review6, review7, review8,
+                review9, review10, review11, review12
+            );
 
         REVIEWS = reviewRepository.saveAll(reviews);
     }
