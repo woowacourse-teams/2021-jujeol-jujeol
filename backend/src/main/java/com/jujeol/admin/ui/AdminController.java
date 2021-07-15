0d8eb376d11a242e8f71c1d2bf5f9668d848aff2
bomin <bomin_93@naver.com>
2021-07-15 19:45:51 +0900

    feat: 어드민 Get요청 완성

--- backend/src/main/java/com/jujeol/admin/ui/AdminController.java
+++ backend/src/main/java/com/jujeol/admin/ui/AdminController.java
@@ -1,9 +1,13 @@
 package com.jujeol.admin.ui;
 
+import com.jujeol.admin.PageResponseAssembler;
 import com.jujeol.admin.ui.dto.AdminDrinkResponse;
+import com.jujeol.commons.dto.CommonResponseDto;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +21,10 @@ public class AdminController {
 
 
     @GetMapping("/drinks")
-    public void drinks(Pageable pageable) {
-        Page<AdminDrinkResponse> adminDrinkResponses = drinkService.findDrinks(pageable);
+    public ResponseEntity<CommonResponseDto<List<AdminDrinkResponse>>> drinks(Pageable pageable) {
+        Page<AdminDrinkResponse> drinkResponses = drinkService.findDrinks(pageable);
+        final CommonResponseDto<List<AdminDrinkResponse>> response = PageResponseAssembler
+                .assemble(drinkResponses);
+        return ResponseEntity.ok(response);
     }
 }
