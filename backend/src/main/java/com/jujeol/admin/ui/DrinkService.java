e20737a169211c97a601e08b229d082ccc6ab637
bomin <bomin_93@naver.com>
2021-07-16 00:21:18 +0900

    feat: 어드민 Get요청 완성2

--- backend/src/main/java/com/jujeol/admin/ui/DrinkService.java
+++ backend/src/main/java/com/jujeol/admin/ui/DrinkService.java
@@ -1,9 +1,13 @@
 package com.jujeol.admin.ui;
 
 import com.jujeol.admin.ui.dto.AdminDrinkResponse;
+import com.jujeol.admin.ui.dto.CategoryResponse;
+import com.jujeol.admin.ui.dto.DrinkRequest;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -13,8 +17,17 @@ public interface DrinkService {
 
     Page<AdminDrinkResponse> findDrinks(Pageable pageable);
 
+    void saveDrinks(List<DrinkRequest> drinkRequests);
+
+    void removeDrink(Long id);
+
+    void updateDrink(Long id, DrinkRequest drinkRequest);
+
     @Service
     class DrinkServiceTemp implements DrinkService {
+
+        private List<AdminDrinkResponse> drinkResponses = new ArrayList<>();
+        private Long id = 8L;
 
         @Override
         public Page<AdminDrinkResponse> findDrinks(Pageable pageable) {
@@ -100,6 +113,35 @@ public interface DrinkService {
                     return null;
                 }
             };
+        }
+
+        @Override
+        public void saveDrinks(List<DrinkRequest> drinkRequests) {
+            final List<AdminDrinkResponse> responses = drinkRequests.stream()
+                    .map(drinkRequest -> new AdminDrinkResponse(
+                            id++,
+                            drinkRequest.getName(),
+                            drinkRequest.getEnglishName(),
+                            drinkRequest.getImageUrl(),
+                            new CategoryResponse(drinkRequest.getCategory().getId(), drinkRequest.getCategory().getName()),
+                            drinkRequest.getAlcoholByVolume())
+                    )
+                    .collect(Collectors.toList());
+            this.drinkResponses.addAll(responses);
+        }
+
+        @Override
+        public void removeDrink(Long id) {
+            drinkResponses.removeIf(adminDrinkResponse -> adminDrinkResponse.getId().equals(id));
+        }
+
+        @Override
+        public void updateDrink(Long id, DrinkRequest drinkRequest) {
+            final AdminDrinkResponse adminDrinkResponse1 = drinkResponses.stream()
+                    .filter(adminDrinkResponse -> adminDrinkResponse.getId().equals(id))
+                    .findAny().orElseThrow(IllegalStateException::new);
+            //TODO : update 만들기
+
         }
     }
 }
