92c405c599552f8330af18ab9a660f935d93de32
sunhpark42 <sunhpark42@gmail.com>
2021-07-16 16:06:53 +0900

    feat: Portal 컴포넌트 구현
    Co-authored-by: EUNHYUN CHO <devhyun637@users.noreply.github.com>

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -2,3 +2,4 @@ export { default as COLOR } from './color';
 export { default as PATH } from './path';
 export { default as Z_INDEX } from './zIndex';
 export { default as PREFERENCE } from './preference';
+export { default as PORTAL_ID } from './portal';
