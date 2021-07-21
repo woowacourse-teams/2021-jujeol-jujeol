814fdfcee9e54618d652274c6cc27b9e8595b51e
sunhpark42 <sunhpark42@gmail.com>
2021-07-21 23:54:27 +0900

    feat: Card 컴포넌트 Props에 onClick 추가

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -2,11 +2,18 @@ import { Container } from './Card.styles';
 
 interface Props extends React.CSSProperties {
   children: React.ReactNode;
+  onClick?: () => void;
 }
 
-const Card = ({ width = '100%', height, padding, backgroundColor, children }: Props) => {
+const Card = ({ width = '100%', height, padding, backgroundColor, children, onClick }: Props) => {
   return (
-    <Container width={width} height={height} padding={padding} backgroundColor={backgroundColor}>
+    <Container
+      width={width}
+      height={height}
+      padding={padding}
+      backgroundColor={backgroundColor}
+      onClick={onClick}
+    >
       {children}
     </Container>
   );
