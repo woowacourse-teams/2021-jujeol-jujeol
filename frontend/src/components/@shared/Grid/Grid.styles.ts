068534782815711ee1ae44f6e82d8f425e72ec6e
sunhpark42 <sunhpark42@gmail.com>
2021-07-21 15:43:27 +0900

    feat: Grid 컴포넌트 구현

---
+++ frontend/src/components/@shared/Grid/Grid.styles.ts
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+
+interface GridStyle extends Omit<React.CSSProperties, 'translate'> {
+  col?: string;
+  row?: string;
+  colGap?: string;
+  rowGap?: string;
+  colMin?: string;
+  colMax?: string;
+  rowMin?: string;
+  rowMax?: string;
+}
+
+const Container = styled.ul<GridStyle>`
+  display: grid;
+
+  ${({ col }) => col && `grid-template-columns: repeat(${col}, 1fr);`}
+  ${({ colGap }) => colGap && `grid-column-gap: ${colGap});`}
+  ${({ colMin, colMax }) =>
+    `grid-template-columns: minmax(${colMin ?? 'auto'}, ${colMax ?? 'auto'});`}
+  ${({ row }) => row && `grid-template-rows: repeat(${row}, 1fr);`}
+  ${({ rowGap }) => rowGap && `grid-row-gap: ${rowGap};`}
+  ${({ rowMin, rowMax }) =>
+    (rowMin || rowMax) && `grid-template-rows: minmax(${rowMin ?? 'auto'}, ${rowMax ?? 'auto'});`}
+`;
+
+export { Container };
+export type { GridStyle };
---
+++ frontend/src/components/@shared/Grid/Grid.tsx
@@ -0,0 +1,24 @@
+import { Container, GridStyle } from './Grid.styles';
+
+interface Props extends GridStyle {
+  children: React.ReactNode;
+}
+
+const Grid = ({ col, row, colGap, rowGap, colMin, colMax, rowMin, rowMax, children }: Props) => {
+  return (
+    <Container
+      col={col}
+      row={row}
+      colGap={colGap}
+      rowGap={rowGap}
+      colMin={colMin}
+      colMax={colMax}
+      rowMin={rowMin}
+      rowMax={rowMax}
+    >
+      {children}
+    </Container>
+  );
+};
+
+export default Grid;
