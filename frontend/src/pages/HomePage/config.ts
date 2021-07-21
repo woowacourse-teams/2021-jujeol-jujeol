bd1388071d6f2c16d18313ea08721b77db0d1e7e
sunhpark42 <sunhpark42@gmail.com>
2021-07-21 19:14:30 +0900

    feat: config 객체를 활용하여 홈 화면 섹션 렌더링

--- frontend/src/pages/HomePage/config.ts
+++ frontend/src/pages/HomePage/config.ts
@@ -5,6 +5,7 @@ interface ItemList {
     theme?: string;
   };
   title: string;
+  titleAlign: 'left' | 'center' | 'right';
   subTitle?: string;
   count?: number;
   isShowMoreEnabled?: boolean;
@@ -13,7 +14,7 @@ interface ItemList {
 interface Banner {
   sectionType: 'BANNER';
   type: 'IMAGE';
-  title: 'Promotion banner';
+  title: '프로모션 배너';
   src: string;
   alt: string;
 }
@@ -28,6 +29,7 @@ const config: HomePageConfig = [
       theme: 'recommendation',
     },
     title: '오늘 이런 술 어때요?',
+    titleAlign: 'center',
     subTitle: '내 취향과 가까운 술을 준비했어요',
     count: 7,
     isShowMoreEnabled: false,
@@ -39,17 +41,18 @@ const config: HomePageConfig = [
       theme: 'recommendation',
     },
     title: '전체보기',
+    titleAlign: 'left',
     count: 3,
     isShowMoreEnabled: true,
   },
   {
     sectionType: 'BANNER',
     type: 'IMAGE',
-    title: 'Promotion banner',
-    src: 'https://github.com/sunhpark42/test_asset/blob/master/jujeol-promotion-cheers.png',
+    title: '프로모션 배너',
+    src: 'https://github.com/sunhpark42/test_asset/blob/master/jujeol-promotion-cheers.png?raw=true',
     alt: '이 건배사 어때요? 이멤버 리멤버!',
   },
 ];
 
 export default config;
-export type { HomePageConfig };
+export type { HomePageConfig, ItemList, Banner };
--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,120 +1,60 @@
-import { Title, ItemList, ItemImage, ItemInfo } from './styles';
-import Card from 'src/components/Card/Card';
-import { useQuery } from 'react-query';
-import API from 'src/apis/requests';
-import { PATH } from 'src/constants';
-import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-import UserContext from 'src/contexts/UserContext';
 import MainHeader from 'src/components/Header/MainHeader';
-import promotionImage from 'src/assets/jujeol-promotion-cheers.png';
-
-interface Drinks {
-  id: number;
-  name: string;
-  imageUrl: string;
-  alcoholByVolume: number;
-}
+import Grid from 'src/components/@shared/Grid/Grid';
+import ItemListSection from 'src/components/Section/ItemListSection';
+import BannerSection from 'src/components/Section/BannerSection';
+import config, { ItemList, Banner } from './config';
 
 const HomePage = () => {
-  const { data: { data: drinks } = [] } = useQuery('drinks', API.getDrinks, {
-    retry: 1,
-  });
-  const { isLoggedIn, userData } = useContext(UserContext);
-
   return (
     <div>
       <MainHeader />
-      <ul
-        style={{ display: 'grid', gridTemplateColumns: 'minmax(320px, 420px)', gridRowGap: '1rem' }}
-      >
-        <li>
-          <section>
-            <Title textAlign="center">
-              <h2>오늘 이런 술 어때요?</h2>
-              <p>
-                {isLoggedIn ? `${userData?.name} 님을 위해 준비했어요` : '주절주절이 엄선한 TOP7'}
-              </p>
-            </Title>
+      <Grid rowGap="2rem" colMin="280px" colMax="420px">
+        {config.map((section: ItemList | Banner) => {
+          if (section.sectionType === 'ITEM_LIST') {
+            const {
+              sectionType,
+              type,
+              title,
+              titleAlign,
+              subTitle,
+              query,
+              isShowMoreEnabled,
+              count,
+            }: ItemList = section;
 
-            <ItemList count={drinks?.length ?? 0}>
-              {drinks?.map(({ id, name, imageUrl, alcoholByVolume }: Drinks) => (
-                <li key={id}>
-                  <Link to={`${PATH.DRINKS}/${id}`}>
-                    <Card width="13rem" height="17rem">
-                      <ItemImage src={imageUrl} alt={name} />
-                      <ItemInfo>
-                        <h3>{name}</h3>
-                        <p>{`${alcoholByVolume}%`}</p>
-                      </ItemInfo>
-                    </Card>
-                  </Link>
-                </li>
-              ))}
-            </ItemList>
-          </section>
-        </li>
-        <li>
-          <section style={{ padding: '0 2rem' }}>
-            <div style={{ display: 'flex', marginBottom: '1rem', justifyContent: 'space-between' }}>
-              <h2 style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>전체보기</h2>
-              <Link to="#">{`더보기 >`}</Link>
-            </div>
-            <ul
-              style={{
-                display: 'grid',
-                gridTemplateColumns: 'repeat(1, 1fr)',
-                gridRowGap: '1rem',
-              }}
-            >
-              {drinks?.slice(0, 3).map(({ id, name, imageUrl, alcoholByVolume }: Drinks) => (
-                <li style={{ display: 'flex' }} key={id}>
-                  <img
-                    src={imageUrl}
-                    alt={name}
-                    width="88px"
-                    style={{ backgroundColor: 'white' }}
-                  />
-                  <div style={{ marginLeft: '1rem' }}>
-                    <h3>{name}</h3>
-                    <p>{`도수 : ${alcoholByVolume}%`}</p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </section>
-        </li>
-        <li>
-          <section>
-            <h2 style={{ opacity: 0 }}>프로모션 이미지</h2>
-            <img src={promotionImage} alt="이 건배사 어때요? 이멤버리멤버!" width="100%" />
-          </section>
-        </li>
-        <li>
-          <section>
-            <Title textAlign="left" style={{ padding: '0 2rem' }}>
-              <h2>막걸리 TOP7</h2>
-              <p>비가 추적추적 내리는 날에는 막걸리!</p>
-            </Title>
+            return (
+              <li key={title}>
+                <ItemListSection
+                  sectionType={sectionType}
+                  type={type as 'CARD' | 'LIST'}
+                  title={title}
+                  titleAlign={titleAlign}
+                  subTitle={subTitle}
+                  query={query}
+                  isShowMoreEnabled={isShowMoreEnabled}
+                  count={count}
+                />
+              </li>
+            );
+          }
 
-            <ItemList count={drinks?.length ?? 0}>
-              {drinks?.map(({ id, name, imageUrl, alcoholByVolume }: Drinks) => (
-                <li key={id}>
-                  <Link to={`${PATH.DRINKS}/${id}`}>
-                    <Card width="13rem" height="17rem">
-                      <ItemImage src={imageUrl} alt={name} />
-                      <ItemInfo>
-                        <h3>{name}</h3>
-                        <p>{`${alcoholByVolume}%`}</p>
-                      </ItemInfo>
-                    </Card>
-                  </Link>
-                </li>
-              ))}
-            </ItemList>
-          </section>
-        </li>
-      </ul>
+          if (section.sectionType === 'BANNER') {
+            const { sectionType, type, title, src, alt }: Banner = section;
+
+            return (
+              <li>
+                <BannerSection
+                  sectionType={sectionType}
+                  type={type as 'IMAGE'}
+                  title={title as '프로모션 배너'}
+                  src={src}
+                  alt={alt}
+                />
+              </li>
+            );
+          }
+        })}
+      </Grid>
     </div>
   );
 };
