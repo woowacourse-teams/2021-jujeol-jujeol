a3114732d4f70fb4e2ae94e551c7822aeafb9c8b
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-07-22 17:07:40 +0900

    feat: Mypage에서 보여지는 개인 리뷰 카드 컴포넌트 마크업 구현

---
+++ frontend/src/components/Card/PersonalReviewCard.tsx
@@ -0,0 +1,26 @@
+import { COLOR } from 'src/constants';
+import Image from '../@shared/Image/Image';
+import TextReview from '../Review/TextReview';
+import Card from './Card';
+
+const PersonalReviewCard = () => {
+  return (
+    <Card
+      width="100%"
+      height="8rem"
+      backgroundColor="transparent"
+      padding="0.6rem"
+      border={`1px solid ${COLOR.PURPLE_100}`}
+    >
+      <Image
+        src="http://placehold.it/100x100"
+        alt="리뷰를 입력한 술"
+        shape="ROUND_SQUARE"
+        size="MEDIUM"
+      />
+      <TextReview />
+    </Card>
+  );
+};
+
+export default PersonalReviewCard;
