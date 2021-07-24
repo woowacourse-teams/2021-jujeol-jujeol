ac5af30ac807fbff7a8e98001a6385c82a9d6ed0
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-07-25 01:55:46 +0900

    refactor: 내 리뷰 카드의 크기 조정

--- frontend/src/components/Card/PersonalReviewCard.tsx
+++ frontend/src/components/Card/PersonalReviewCard.tsx
@@ -7,16 +7,15 @@ const PersonalReviewCard = () => {
   return (
     <Card
       width="100%"
-      height="8rem"
       backgroundColor="transparent"
-      padding="0.6rem"
+      padding="0.6rem 1.3rem 0.6rem 0.6rem"
       border={`1px solid ${COLOR.PURPLE_100}`}
     >
       <Img
         src="http://placehold.it/100x100"
         alt="리뷰를 입력한 술"
         shape="ROUND_SQUARE"
-        size="MEDIUM"
+        size="SMALL"
       />
       <TextReview />
     </Card>
