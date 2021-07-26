01a6f2820b288fd5ef3c9bbc6684881b3695f6a0
sihyung92 <fjzjqhdl@gmail.com>
2021-07-26 20:09:06 +0900

    chore: merge conflict 수정

--- backend/src/main/java/com/jujeol/member/ui/dto/MemberRequest.java
+++ backend/src/main/java/com/jujeol/member/ui/dto/MemberRequest.java
@@ -1,7 +1,5 @@
 package com.jujeol.member.ui.dto;
 
-import com.jujeol.member.application.dto.MemberDto;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
