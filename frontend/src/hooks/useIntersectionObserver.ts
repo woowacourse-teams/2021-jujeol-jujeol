a6605b641c6e4b4072d2fe966f915330936aaf31
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-07-27 07:39:54 +0900

    feat: useIntersectionObserver 커스텀 훅 구현

---
+++ frontend/src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,35 @@
+import { RefObject, useEffect } from 'react';
+
+const useIntersectionObserver = ({
+  target,
+  fetchNextPage,
+  hasNextPage,
+}: {
+  target: RefObject<HTMLDivElement>;
+  fetchNextPage: () => void;
+  hasNextPage: boolean | undefined;
+}) => {
+  useEffect(() => {
+    const targetElement = target.current;
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
+    });
+
+    if (targetElement) {
+      observer.observe(targetElement);
+    }
+
+    return () => {
+      if (targetElement) {
+        observer.unobserve(targetElement);
+      }
+    };
+  }, [target.current]);
+};
+
+export default useIntersectionObserver;
