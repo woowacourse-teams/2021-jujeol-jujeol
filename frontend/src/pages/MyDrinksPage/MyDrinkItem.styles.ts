207f5248968baa17a163a57a44123f058eb4da4c
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-07-27 08:56:07 +0900

    refactor: 필요없는 컴포넌트 병합 및 상품 클릭 시 상세페이지로 이동하는 기능 추가

--- frontend/src/pages/MyDrinksPage/MyDrinkItem.styles.ts
+++ frontend/src/pages/MyDrinksPage/MyDrinkItem.styles.ts
@@ -1,6 +1,35 @@
 import styled from '@emotion/styled';
 import Flex from 'src/styles/Flex';
 
+const Container = styled.li`
+  ${Flex({ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' })};
+
+  :last-child {
+    position: relative;
+    &::after {
+      content: '';
+      position: absolute;
+      right: -1.5rem;
+      width: 1.5rem;
+      height: 100%;
+    }
+  }
+
+  p {
+    margin-top: 0.5rem;
+
+    font-size: 0.95rem;
+    line-height: 1.5;
+
+    overflow: hidden;
+    display: -webkit-box;
+
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    text-overflow: ellipsis;
+  }
+`;
+
 const Description = styled.div`
   ${Flex({ justifyContent: 'center', alignItems: 'center' })};
 
@@ -11,4 +40,4 @@ const Description = styled.div`
   }
 `;
 
-export { Description };
+export { Container, Description };
--- frontend/src/pages/MyDrinksPage/MyDrinkItem.tsx
+++ frontend/src/pages/MyDrinksPage/MyDrinkItem.tsx
@@ -1,25 +1,33 @@
-import { useEffect } from 'react';
-import { ImageSizeType } from 'src/components/@shared/Image/Image';
+import { useHistory } from 'react-router-dom';
+import { ImageSizeType, Img } from 'src/components/@shared/Image/Image';
 import StarIcon from 'src/components/Icons/star';
-import VerticalItem from 'src/components/Item/VerticalItem';
-import { COLOR } from 'src/constants';
-import { Description } from './MyDrinkItem.styles';
+import { COLOR, PATH } from 'src/constants';
+import { Container, Description } from './MyDrinkItem.styles';
 
 interface Props {
   size: ImageSizeType;
   drink: MyDrink.MyDrinkItem;
+  onClick?: () => void;
 }
 
 const MyDrinkItem = ({ size, drink }: Props) => {
   const { imageUrl, name, preferenceRate } = drink;
 
+  const history = useHistory();
+
   return (
-    <VerticalItem src={imageUrl} alt={name} shape="ROUND_SQUARE" size={size} title={name}>
+    <Container
+      onClick={() => {
+        history.push(`${PATH.DRINKS}/${drink.id}`);
+      }}
+    >
+      <Img src={imageUrl} alt={name} shape="ROUND_SQUARE" size={size} />
+      <p>{name}</p>
       <Description>
         <StarIcon width="0.8rem" color={COLOR.YELLOW_300} />
         <span>{preferenceRate}</span>
       </Description>
-    </VerticalItem>
+    </Container>
   );
 };
 
