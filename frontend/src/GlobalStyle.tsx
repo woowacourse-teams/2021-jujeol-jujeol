ba8d84fb7bc4a0dd00957214c7831159587d08cf
sunhpark42 <sunhpark42@gmail.com>
2021-07-30 13:03:29 +0900

    feat: button disabled 스타일 추가

--- frontend/src/GlobalStyle.tsx
+++ frontend/src/GlobalStyle.tsx
@@ -101,6 +101,11 @@ const GlobalStyle = () => (
         color: ${COLOR.BLACK_900};
         font-size: 16px;
         -webkit-tap-highlight-color: transparent;
+
+        :disabled {
+          color: ${COLOR.GRAY_300};
+          filter: grayscale(100%);
+        }
       }
 
       input {
