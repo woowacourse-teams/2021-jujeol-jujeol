a62a043f1bcce9e9711e5acb7d9766e03b9e2ce6
sunhpark42 <sunhpark42@gmail.com>
2021-07-30 16:28:09 +0900

    fix: css import 오류 수정

--- frontend/src/components/Card/ReviewCard.styles.ts
+++ frontend/src/components/Card/ReviewCard.styles.ts
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import css from '@emotion/css';
+import { css } from '@emotion/react';
 import { COLOR } from 'src/constants';
 
 const Header = styled.div`
--- frontend/src/components/Modal/Modal.styles.ts
+++ frontend/src/components/Modal/Modal.styles.ts
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled';
-import css from '@emotion/css';
-import { keyframes } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
 import { COLOR, Z_INDEX } from 'src/constants';
 
 const popUp = keyframes`
