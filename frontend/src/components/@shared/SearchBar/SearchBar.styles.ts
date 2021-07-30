89a3405a63ef10bc854eefe6c329a5dc2158c489
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-07-31 06:15:02 +0900

    feat: SearchBar 컴포넌트 확장 및 Search Icon 구현

--- frontend/src/components/@shared/SearchBar/SearchBar.styles.ts
+++ frontend/src/components/@shared/SearchBar/SearchBar.styles.ts
@@ -1,47 +1,98 @@
+import Flex from 'src/styles/Flex';
 import styled from '@emotion/styled';
 import { COLOR } from 'src/constants';
 
-const Container = styled.form`
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0.5rem 2rem;
-  width: 80%;
+const Container = styled.form<Omit<React.CSSProperties, 'translate'>>`
+  ${Flex({ justifyContent: 'center', alignItems: 'center' })};
+  width: ${({ width }) => width && `${width}%`};
   height: 3rem;
+  padding: ${({ padding }) => padding && `${padding}`};
+  margin: 1.5rem auto;
   background-color: rgba(255, 255, 255, 0.3);
-  border-radius: 2rem;
   border: 2px solid white;
+  border-radius: 2rem;
   position: relative;
-
-  input {
-    width: 100%;
-    height: 100%;
-    color: ${COLOR.WHITE_100};
-    border: 0;
-    outline: none;
+  button:first-of-type {
     background-color: transparent;
-    font-size: 1rem;
-    padding: 0.2rem 0.2rem;
-  }
-
-  input::placeholder {
-    font-size: 1rem;
-    color: ${COLOR.GRAY_200};
-  }
-
-  button {
-    width: 2rem;
-    height: 2rem;
-    border-radius: 1rem;
     border: 0;
-    background-color: ${COLOR.GRAY_200};
-
+    margin-left: -1rem;
+  }
+  > svg {
     position: absolute;
-    right: 0.3rem;
+    left: 1rem;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  > div {
+    position: absolute;
+    right: 0.5rem;
     top: 50%;
     transform: translateY(-50%);
   }
 `;
 
-export { Container };
+const SearchInput = styled.input<Omit<React.CSSProperties, 'translate'>>`
+  width: 100%;
+  height: 100%;
+  padding-right: ${({ paddingRight }) => paddingRight && `${paddingRight}`};
+  background-color: transparent;
+  border: 0;
+  outline: none;
+  font-size: 1rem;
+  font-weight: 700;
+  text-align: ${({ textAlign }) => textAlign && `${textAlign}`};
+  color: ${COLOR.WHITE_100};
+  ::placeholder {
+    font-size: 1rem;
+    line-height: 1.5;
+    color: ${COLOR.GRAY_200};
+  }
+  // search type일 때 제공되는 reset 버튼을 삭제하는 코드
+  ::-ms-clear,
+  ::-ms-reveal {
+    display: none;
+    width: 0;
+    height: 0;
+  }
+  ::-webkit-search-decoration,
+  ::-webkit-search-cancel-button,
+  ::-webkit-search-results-button,
+  ::-webkit-search-results-decoration {
+    display: none;
+  }
+`;
+
+const ResetButton = styled.button`
+  ${Flex({ justifyContent: 'center', alignItems: 'center' })};
+  height: 100%;
+  background-color: transparent;
+  border: 0;
+  position: absolute;
+  right: 2.5rem;
+  span {
+    background-color: ${COLOR.GRAY_200};
+    color: ${COLOR.WHITE_200};
+    border-radius: 50%;
+    width: 1.125rem;
+    height: 1.125rem;
+    font-size: 0.7rem;
+    font-weight: 900;
+    line-height: 1.7;
+    vertical-align: middle;
+  }
+`;
+
+const SearchButton = styled.button`
+  ${Flex({ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' })}
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+  border: 0;
+  background-color: ${COLOR.GRAY_200};
+  position: absolute;
+  right: 0.5rem;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+export { Container, SearchInput, ResetButton, SearchButton };
--- frontend/src/components/@shared/SearchBar/SearchBar.tsx
+++ frontend/src/components/@shared/SearchBar/SearchBar.tsx
@@ -1,14 +1,70 @@
-import { InputHTMLAttributes } from 'react';
-import { Container } from './SearchBar.styles';
+import { InputHTMLAttributes, ChangeEvent, useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import SearchIcon from 'src/components/Icons/search';
+import { COLOR, PATH } from 'src/constants';
+import Arrow from '../Arrow/Arrow';
+import { Container, ResetButton, SearchButton, SearchInput } from './SearchBar.styles';
 
 const SearchBar = ({
   onSubmit,
+  onClick,
   placeholder,
 }: InputHTMLAttributes<HTMLInputElement | HTMLFormElement>) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const [isMainPage, setIsMainPage] = useState(true);
+  const [value, setValue] = useState('');
+
+  const onMoveGoBack = () => history.goBack();
+
+  useEffect(() => {
+    if (location.pathname === PATH.HOME || location.pathname == PATH.ROOT) {
+      setIsMainPage(true);
+    } else {
+      setIsMainPage(false);
+    }
+  }, []);
+
+  const onInputWords = (event: ChangeEvent<HTMLInputElement>) => setValue(event.target.value);
+  const onResetInput = () => setValue('');
+
   return (
-    <Container onSubmit={onSubmit}>
-      <input type="search" placeholder={placeholder} />
-      <button>검색</button>
+    <Container
+      width={isMainPage ? '80' : '90'}
+      padding={isMainPage ? '0.5rem 1.5rem' : '0.5rem 1.5rem'}
+      onSubmit={onSubmit}
+      onClick={onClick}
+    >
+      {isMainPage ? (
+        <SearchIcon color={COLOR.WHITE_200} width="1.2rem" />
+      ) : (
+        <button type="button" onClick={onMoveGoBack}>
+          <Arrow size="0.7rem" borderWidth="2px" dir="LEFT" />
+        </button>
+      )}
+      <SearchInput
+        type="search"
+        value={value}
+        placeholder={placeholder}
+        paddingRight={isMainPage ? '0' : '3.5rem'}
+        textAlign={isMainPage ? 'center' : 'left'}
+        onChange={onInputWords}
+        required
+      />
+      {!isMainPage && (
+        <>
+          {value && (
+            <ResetButton type="reset" onClick={onResetInput}>
+              <span>X</span>
+            </ResetButton>
+          )}
+          <SearchButton type="submit">
+            <SearchIcon color={COLOR.GRAY_400} width="1.2rem" />
+          </SearchButton>
+        </>
+      )}
     </Container>
   );
 };
