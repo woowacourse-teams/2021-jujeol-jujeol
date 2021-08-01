3d7650291549c10ec63c0dea47c8edda2178f22f
bomin <bomin_93@naver.com>
2021-08-01 21:14:29 +0900

    fix: 여러번 날아가는 쿼리를 임시방편으로 EAGER 로 수정

--- backend/src/main/java/com/jujeol/drink/domain/ViewCount.java
+++ backend/src/main/java/com/jujeol/drink/domain/ViewCount.java
@@ -2,6 +2,7 @@ package com.jujeol.drink.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,7 +25,7 @@ public class ViewCount {
     private Long id;
     @Column
     private Long viewCount;
-    @OneToOne(mappedBy = "viewCount")
+    @OneToOne(mappedBy = "viewCount", fetch = FetchType.EAGER)
     private Drink drink;
 
     public static ViewCount create(Long viewCount) {
