9bfb06e21dfe7bd00cdd8c5269389af9c757c0cf
soulG <bssj9307@gmail.com>
2021-08-05 11:09:39 +0900

    fix: timezone 관련 설정 추가 (#267)

--- backend/src/main/java/com/jujeol/JujeolApplication.java
+++ backend/src/main/java/com/jujeol/JujeolApplication.java
@@ -1,5 +1,7 @@
 package com.jujeol;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +9,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class JujeolApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JujeolApplication.class, args);
