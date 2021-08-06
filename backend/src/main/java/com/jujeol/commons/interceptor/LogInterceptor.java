6efcd9e0f028c133739ccd120d5f87724070ce35
Solji Choi <bssj9307@gmail.com>
2021-08-06 18:04:39 +0900

    refactor: WebMvcConfig 리팩토링, LogInterceptor 추가 - LoginInterceptor 를 WebMvcConfig 로 병합 - 요청 시작점 log 로 출력

---
+++ backend/src/main/java/com/jujeol/commons/interceptor/LogInterceptor.java
@@ -0,0 +1,26 @@
+package com.jujeol.commons.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LogInterceptor implements HandlerInterceptor {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+            Object handler) {
+        logger.info("----------------------- \"{}\" Start -----------------------", request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) {
+        logger.info("----------------------- \"{}\" End -----------------------", request.getRequestURI());
+    }
+}
