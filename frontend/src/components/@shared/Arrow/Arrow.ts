b046a269662ad36cda7630c2b15ef5307e216ad6
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-08-07 21:26:18 +0900

    refactor: Arrow 컴포넌트에 color Props 추가

--- frontend/src/components/@shared/Arrow/Arrow.ts
+++ frontend/src/components/@shared/Arrow/Arrow.ts
@@ -19,12 +19,15 @@ const Arrow = styled.div<Omit<ArrowStyle, 'translate'>>`
   line-height: 1.5;
 
   display: inline-block;
-
   width: ${({ size }) => size ?? '0.7rem'};
   height: ${({ size }) => size ?? '0.7rem'};
 
-  border-top: ${({ borderWidth }) => borderWidth ?? '0.125rem'} solid ${COLOR.WHITE_200};
-  border-right: ${({ borderWidth }) => borderWidth ?? '0.125rem'} solid ${COLOR.WHITE_200};
+  border-top: ${({ borderWidth }) => borderWidth ?? '0.125rem'};
+  border-right: ${({ borderWidth }) => borderWidth ?? '0.125rem'};
+  border-top-color: ${({ color }) => color ?? `${COLOR.WHITE_200}`};
+  border-right-color: ${({ color }) => color ?? `${COLOR.WHITE_200}`};
+  border-top-style: solid;
+  border-right-style: solid;
   transform: ${({ dir }) => `rotate(${DIRECTION[dir]})`};
 `;
 
