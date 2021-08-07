a0a41726122566d1a0df763d9f6851a1e32857eb
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-08-07 15:37:10 +0900

    feat: 모바일에서 모달 끌어서 닫는 기능 구현

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler } from 'react';
+import { TouchEventHandler, MouseEventHandler } from 'react';
 import { Container, Content } from './Modal.styles';
 
 interface Props {
@@ -7,6 +7,12 @@ interface Props {
   children: React.ReactNode;
 }
 
+const modalPosition = {
+  contentHeight: window.innerHeight * 0.8,
+  currentYPosition: 0,
+  requestAnimationFrameKey: 0,
+};
+
 const Modal = ({ isOpened, setIsOpened, children }: Props) => {
   const onClose: MouseEventHandler<HTMLDivElement> = (event) => {
     if (event.currentTarget === event.target) {
@@ -14,9 +20,44 @@ const Modal = ({ isOpened, setIsOpened, children }: Props) => {
     }
   };
 
+  const onTouchStart: TouchEventHandler<HTMLDivElement> = (event) => {
+    modalPosition.currentYPosition = event.touches[0].clientY;
+  };
+
+  const onTouchMove: TouchEventHandler<HTMLDivElement> = (event) => {
+    const contentStyle = event.currentTarget.style;
+    const changeYPosition = event.touches[0].clientY - modalPosition.currentYPosition;
+
+    if (changeYPosition > 0) {
+      modalPosition.requestAnimationFrameKey = requestAnimationFrame(() => {
+        contentStyle.transform = `translateY(${changeYPosition}px)`;
+      });
+    }
+
+    if (changeYPosition > modalPosition.contentHeight * 0.5) {
+      closeModalByTouch();
+    }
+  };
+
+  const closeModalByTouch = () => {
+    setIsOpened(false);
+    cancelAnimationFrame(modalPosition.requestAnimationFrameKey);
+  };
+
+  const onTouchEnd: TouchEventHandler<HTMLDivElement> = (event) => {
+    event.currentTarget.style.transform = 'none';
+  };
+
   return (
     <Container isOpened={isOpened} onClick={onClose}>
-      <Content isOpened={isOpened}>{children}</Content>
+      <Content
+        isOpened={isOpened}
+        onTouchStart={onTouchStart}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}
+      >
+        {children}
+      </Content>
     </Container>
   );
 };
