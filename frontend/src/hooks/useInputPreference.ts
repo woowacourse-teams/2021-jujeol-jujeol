5b6d29c118ca4b67d391d34f5602b97829c584b9
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-08-12 02:33:45 +0900

    feat: 선호도 입력을 안내하는 useInputPreference 커스텀 훅 생성

---
+++ frontend/src/hooks/useInputPreference.ts
@@ -0,0 +1,38 @@
+import { useState, RefObject } from 'react';
+
+const useNoticeToInputPreference = ({ targetRef }: { targetRef: RefObject<HTMLDivElement> }) => {
+  const [isBlinked, setIsBlinked] = useState(false);
+
+  const scrollToPreferenceSection = () => {
+    targetRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          scrollToPreferenceSection();
+        } else {
+          observer.disconnect();
+        }
+
+        setIsBlinked(true);
+      });
+    },
+    { threshold: 1.0 }
+  );
+
+  const observePreferenceSection = () => {
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    setTimeout(() => {
+      setIsBlinked(false);
+    }, 3000);
+  };
+
+  return { isBlinked, setIsBlinked, observePreferenceSection };
+};
+
+export default useNoticeToInputPreference;
