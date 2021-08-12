82cd45f0ed556f695bbcc89ae12942301c2c422d
SungSiHyung <51393021+sihyung92@users.noreply.github.com>
2021-08-12 11:30:35 +0900

    feat: 건배사 랜덤아이디 추가 (#331)

--- backend/src/main/java/com/jujeol/member/member/domain/nickname/InmemoryCheersExpression.java
+++ backend/src/main/java/com/jujeol/member/member/domain/nickname/InmemoryCheersExpression.java
@@ -7,9 +7,18 @@ import lombok.Getter;
 
 @Getter
 public enum InmemoryCheersExpression {
-    BLUE_JEAN("청춘은 바로 지금", "청바지"),
-    SAUNA("사랑과 우정을 나누자", "사우나"),
-    WILD_FLOWER("야심차고 생생하고 화끈하게", "야생화");
+    BLUE_JEAN("청춘은 바로 지금!", "청바지"),
+    SAUNA("사랑과 우정을 나누자!", "사우나"),
+    WILD_FLOWER("야심차고 생생하고 화끈하게!", "야생화"),
+    CIDAR("사랑합니다 이평생 다바쳐서!", "사이다"),
+    DONG_SAMUSO("동료를 사랑하는 것이 무엇보다 소중하다!", "동사무소"),
+    HAN_WOO("한마음으로 우리는 갈수록 비상한다!", "한우갈비"),
+    CHAN_CHAN("희망찬 활기찬 가득찬!", "찬찬찬"),
+    JIN_DAL_RAE("진하고 달콤한 내일을 위하여!", "진달래"),
+    DSHONG_MASHONG("드숑! 마숑!", "드숑마숑"),
+    UHAHA("우리는 하늘 아래 하나다!", "우하하"),
+    BYUN_SADDO("변함없는 사랑으로 내일 또 만납시다!", "변사또"),
+    BAKKAS("박력있게 카리스마있게 스피디하게 원샷!", "박카스");
 
     InmemoryCheersExpression(String expression, String abbreviation) {
         this.cheersExpression = new CheersExpression(expression, abbreviation);
