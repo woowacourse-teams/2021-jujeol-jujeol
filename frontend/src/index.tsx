4d66d643f51ff33c0e60383789fbf052f904840c
sunhpark42 <67677561+sunhpark42@users.noreply.github.com>
2021-08-12 15:40:48 +0900

    feat: prod 환경에서 console 삭제 및 sentry 적용 (#335)
    * feat: prod환경에서 콘솔에 에러가 노출되지 않게 logger 설정
    
    * feat: sentry 적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,31 @@ import GlobalStyle from './GlobalStyle';
 import dotenv from 'dotenv';
 import { UserProvider } from './contexts/UserContext';
 
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+import { setLogger } from 'react-query';
+
 dotenv.config();
+
+if (process.env.SNOWPACK_PUBLIC_ENV === 'PROD') {
+  Sentry.init({
+    dsn: process.env.SNOWPACK_PUBLIC_SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+
+  setLogger({
+    log: (message) => {
+      Sentry.captureMessage(message);
+    },
+    warn: (message) => {
+      Sentry.captureMessage(message);
+    },
+    error: (error) => {
+      Sentry.captureException(error);
+    },
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
