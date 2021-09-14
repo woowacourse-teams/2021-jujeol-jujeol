44899e2798039a8806f4e9d2f2c7f1d72a5b9527
SungSiHyung <51393021+sihyung92@users.noreply.github.com>
2021-09-14 15:26:15 +0900

    feat: 관리자도구 상세정보 입력기능 추가 (#352)
    * feat: admin 페이지 칼럼 수정 작업 중간 커밋
    
    * feat: table 스타일 정리
    
    * feat: 모달에 상세설명 칼럼 추가
    
    * refactor: methodFetches의 response 처리 코드 리팩토링
    
    * feat: 관리자도구에 description insert 추가
    
    Co-authored-by: sunhpark42 <sunhpark42@gmail.com>

--- backend/src/main/resources/static/admin/js/drinks/model/Drink.js
+++ backend/src/main/resources/static/admin/js/drinks/model/Drink.js
@@ -1,10 +1,11 @@
 export default class Drink {
-  constructor(id, name, englishName = "", alcoholByVolume, imageUrl, category) {
+  constructor(id, name, englishName = "", alcoholByVolume, imageUrl, description, category) {
     this.id = id;
     this.name = name;
     this.englishName = englishName;
     this.alcoholByVolume = alcoholByVolume;
     this.imageUrl = imageUrl;
+    this.description = description;
     this.category = category;
     this.onUpdate = false;
   }
