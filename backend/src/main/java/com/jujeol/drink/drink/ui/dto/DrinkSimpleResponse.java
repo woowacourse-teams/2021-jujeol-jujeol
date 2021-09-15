9546ddc80da906ea8e6d2db2b30d1a0d716dff60
pika96 <yjwdbswldn@naver.com>
2021-09-15 15:22:09 +0900

    feat: 피드백 반영

--- backend/src/main/java/com/jujeol/drink/drink/ui/dto/DrinkSimpleResponse.java
+++
@@ -1,2 +0,0 @@
-package com.jujeol.drink.drink.ui.dto;public class DrinkSimpleResponse {
-}
--- backend/src/main/resources/db/migration/prod/V3__drink_add_imageFilePath.sql
+++ backend/src/main/resources/db/migration/prod/V3__drink_add_imageFilePath.sql
@@ -1,4 +1,4 @@
 alter table drink drop imageFilePath;
-alter table drink add column smallImageFilePath
-alter table drink add column mediumImageFilePath
-alter table drink add column largeImageFilePath
+alter table drink add column smallImageFilePath varchar(1000);
+alter table drink add column mediumImageFilePath varchar(1000);
+alter table drink add column largeImageFilePath varchar(1000);
