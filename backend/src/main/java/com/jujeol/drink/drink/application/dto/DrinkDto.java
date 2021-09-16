bb5aeed7b67929acdf6e5342b02433b73f1a4c32
bomin <bomin_93@naver.com>
2021-09-16 23:08:52 +0900

    refactor: search 나의 선호도 추가

--- backend/src/main/java/com/jujeol/drink/drink/application/dto/DrinkDto.java
+++ backend/src/main/java/com/jujeol/drink/drink/application/dto/DrinkDto.java
@@ -54,4 +54,8 @@ public class DrinkDto {
     ) {
         return create(drink, preference, 0);
     }
+
+    public void addPreferenceRate(Preference preference) {
+        preferenceRate = preference.getRate();
+    }
 }
