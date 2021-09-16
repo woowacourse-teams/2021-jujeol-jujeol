d1aa8b06cac2a86cea3fe5f1904f21d352a1e45d
sihyung92 <fjzjqhdl@gmail.com>
2021-09-16 15:18:52 +0900

    feat & chore: 주석 삭제 및 테스트 버킷 주소 추가

--- backend/src/main/java/com/jujeol/aws/service/ImageService.java
+++ backend/src/main/java/com/jujeol/aws/service/ImageService.java
@@ -44,13 +44,5 @@ public class ImageService {
         }
         return convertedFile;
     }
-    // 1. 멀티파트 업로드 요청
-    // 2. 파일 받아서 이미지 리사이징
-    // 3. s3 업로드
-    // 4. url 을 db 저장 (drink -> imageUrl 수정)
 
-    // test 짜기
-    // 리사이징 로직 작성 , 파일을 3개로 늘려야 함
-    // s3에 업로드하고, db에 url로 저장
-    // 이미지 url 을 클라이언트에 응답
 }
