880ad07d1c60e2a1777a775b18ab244068c97abb
bomin <bomin_93@naver.com>
2021-09-16 17:24:55 +0900

    fix: expectedPreference를 랜덤으로 반환하는 거를 실제 가중치로 반환

--- backend/src/main/java/com/jujeol/drink/recommend/domain/RecommendForMember.java
+++ backend/src/main/java/com/jujeol/drink/recommend/domain/RecommendForMember.java
@@ -1,8 +1,8 @@
 package com.jujeol.drink.recommend.domain;
 
-import com.jujeol.drink.drink.domain.Drink;
 import com.jujeol.drink.drink.domain.repository.DrinkRepository;
 import com.jujeol.drink.recommend.application.RecommendStrategy;
+import com.jujeol.drink.recommend.infrastructure.RecommendationResponse;
 import com.jujeol.drink.recommend.infrastructure.RecommendationSystem;
 import com.jujeol.preference.domain.Preference;
 import com.jujeol.preference.domain.PreferenceRepository;
@@ -20,53 +20,74 @@ public class RecommendForMember implements RecommendStrategy {
     private final PreferenceRepository preferenceRepository;
 
     @Override
-    public List<Drink> recommend(String category, Long memberId, int pageSize) {
+    public List<RecommendedDrinkResponse> recommend(String category, Long memberId, int pageSize) {
         List<Preference> preferences;
-        if(category == null) {
+        if (category == null) {
             preferences = preferenceRepository.findAll();
-        }else {
+        } else {
             preferences = preferenceRepository.findAllByCategory(category);
         }
         final List<Preference> myPreferences = preferences.stream()
                 .filter(preference -> preference.getMember().getId().equals(memberId))
                 .collect(Collectors.toList());
-        final List<Long> itemIds = recommendationSystem.recommend(memberId, pageSize, preferences);
+        final List<RecommendationResponse> recommendedItems = recommendationSystem
+                .recommend(memberId, pageSize, preferences);
 
-        List<Drink> drinks = drinkRepository.findAllById(itemIds);
-        drinks = setFirstNoTriedItem(drinks, myPreferences);
+        List<RecommendedDrinkResponse> drinks = recommendedItems.stream()
+                .map(res -> new RecommendedDrinkResponse(drinkRepository.getById(res.getItemId()),
+                        res.getExpectedPreference()))
+                .collect(Collectors.toList());
 
-        if (itemIds.size() < pageSize) {
+        drinks = sortByNoTriedItem(drinks, myPreferences);
+
+        if (recommendedItems.size() < pageSize) {
             addItemByPreferenceAvg(drinks, myPreferences, memberId, pageSize, category);
         }
 
         return drinks;
     }
 
-    private void addItemByPreferenceAvg(List<Drink> drinks, List<Preference> myPreferences,
+    private void addItemByPreferenceAvg(List<RecommendedDrinkResponse> drinks,
+            List<Preference> myPreferences,
             Long memberId, int pageSize, String category) {
-        List<Drink> drinksByPreference = drinkRepository
-                .findDrinksForMember(memberId, Pageable.ofSize(pageSize), category);
-        drinksByPreference = setFirstNoTriedItem(drinksByPreference, myPreferences);
-        for (Drink drink : drinksByPreference) {
+        List<RecommendedDrinkResponse> drinksByPreference;
+        if (category != null) {
+            drinksByPreference = drinkRepository
+                    .findDrinksForMember(memberId, Pageable.ofSize(pageSize), category)
+                    .stream()
+                    .map(drink -> new RecommendedDrinkResponse(drink, 0))
+                    .collect(Collectors.toList());
+        } else {
+            drinksByPreference = drinkRepository
+                    .findDrinksForMember(memberId, Pageable.ofSize(pageSize))
+                    .stream()
+                    .map(drink -> new RecommendedDrinkResponse(drink, 0))
+                    .collect(Collectors.toList());
+        }
+
+        drinksByPreference = sortByNoTriedItem(drinksByPreference, myPreferences);
+        for (RecommendedDrinkResponse drink : drinksByPreference) {
             if (drinks.size() < pageSize && !drinks.contains(drink)) {
                 drinks.add(drink);
             }
         }
     }
 
-    private List<Drink> setFirstNoTriedItem(List<Drink> drinks, List<Preference> myPreferences) {
-        LinkedList<Drink> triedDrinks = new LinkedList<>();
-        for (Drink drink : drinks) {
+    private List<RecommendedDrinkResponse> sortByNoTriedItem(List<RecommendedDrinkResponse> drinks,
+            List<Preference> myPreferences) {
+        LinkedList<RecommendedDrinkResponse> triedDrinks = new LinkedList<>();
+        for (RecommendedDrinkResponse drink : drinks) {
             addDrinkByCheckingTriedOrNot(myPreferences, triedDrinks, drink);
         }
         return triedDrinks;
     }
 
     private void addDrinkByCheckingTriedOrNot(List<Preference> myPreferences,
-            LinkedList<Drink> triedDrinks,
-            Drink drink) {
+            LinkedList<RecommendedDrinkResponse> triedDrinks,
+            RecommendedDrinkResponse drink) {
         myPreferences.stream()
-                .filter(preference -> preference.getDrink().getId().equals(drink.getId()))
+                .filter(preference -> preference.getDrink().getId()
+                        .equals(drink.getDrink().getId()))
                 .findAny()
                 .ifPresentOrElse(preference -> {
                     if (preference.getRate() > 3) {
