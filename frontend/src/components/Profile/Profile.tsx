01628eb7f814c234378c02a25d7a0c6a6632f2fa
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-09-16 22:20:24 +0900

    feat: 닉네임 수정 버튼 생성

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,5 +1,6 @@
 import { ImgHTMLAttributes } from 'react';
-import { Container } from './Profile.styles';
+import { EditIcon } from '../@shared/Icons';
+import { Container, EditButton } from './Profile.styles';
 
 interface Props extends ImgHTMLAttributes<HTMLImageElement> {
   ProfileIcon: (props: IconProps) => React.ReactElement;
@@ -8,12 +9,19 @@ interface Props extends ImgHTMLAttributes<HTMLImageElement> {
 }
 
 const Profile = ({ ProfileIcon, nickname = '', bio = '' }: Props) => {
+  const onEditModalOpen = () => {
+    console.log('edit Button');
+  };
+
   return (
     <Container>
-      <ProfileIcon />
+      <ProfileIcon width="72px" height="72px" />
 
       <div>
         <h3>{nickname}</h3>
+        <EditButton onClick={onEditModalOpen}>
+          <EditIcon width="1.5rem" height="1.5rem" />
+        </EditButton>
         <p>{bio}</p>
       </div>
     </Container>
