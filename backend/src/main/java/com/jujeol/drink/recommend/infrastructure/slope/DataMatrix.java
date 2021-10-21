4419c4488a73191ee5e33c2d77a2f8bb5ef3f0e8
bomin <bomin_93@naver.com>
2021-10-21 19:52:25 +0900

    feat: o(n3) 에서 o(n2) 로 수정

--- backend/src/main/java/com/jujeol/drink/recommend/infrastructure/slope/DataMatrix.java
+++ backend/src/main/java/com/jujeol/drink/recommend/infrastructure/slope/DataMatrix.java
@@ -1,47 +1,48 @@
 package com.jujeol.drink.recommend.infrastructure.slope;
 
-import static java.util.stream.Collectors.groupingBy;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class DataMatrix {
 
     private final Map<Long, Map<Long, ItemCounter>> matrix;
-    private final Map<Long, List<DataModel>> dataByMember;
+    private final Map<Long, Map<Long, Double>> dataByMember;
 
     public DataMatrix(List<DataModel> dataModel) {
-        this.dataByMember = groupingByMember(dataModel);
+        this.dataByMember = new HashMap<>();
         this.matrix = new HashMap<>();
-
-        for (List<DataModel> dataByMember : dataByMember.values()) {
-            for (DataModel model : dataByMember) {
-                final Long itemKey = model.getItemId();
-                matrix.computeIfAbsent(itemKey, key -> new HashMap<>());
-                for (DataModel model2 : dataByMember) {
-                    if (itemKey.equals(model2.getItemId())) {
-                        continue;
-                    }
-                    final Map<Long, ItemCounter> eachItem = matrix.get(itemKey);
-                    final ItemCounter itemCounter =
-                            eachItem.computeIfAbsent(model2.getItemId(), key -> new ItemCounter());
-                    itemCounter.addSum(model.getPreference() - model2.getPreference());
-                }
-            }
-        }
+        groupingByMember(dataModel);
     }
 
-    private Map<Long, List<DataModel>> groupingByMember(List<DataModel> dataModel) {
-        return dataModel.stream()
-                .collect(groupingBy(DataModel::getMemberId));
+    private void groupingByMember(List<DataModel> dataModel) {
+        for (DataModel model : dataModel) {
+            final Long memberId = model.getMemberId();
+            final Map<Long, Double> itemByMember = dataByMember
+                    .computeIfAbsent(memberId, id -> new HashMap<>());
+
+            for (Entry<Long, Double> itemPreference : itemByMember.entrySet()) {
+                final Long itemId = itemPreference.getKey();
+                final Double preference = itemPreference.getValue();
+                if(itemId.equals(model.getItemId())) continue;
+
+                final Map<Long, ItemCounter> primaryMap =
+                        matrix.computeIfAbsent(model.getItemId(), id -> new HashMap<>());
+                final Map<Long, ItemCounter> secondaryMap =
+                        matrix.computeIfAbsent(itemId, id -> new HashMap<>());
+
+                primaryMap.computeIfAbsent(itemId, id -> new ItemCounter()).addSum(model.getPreference() - preference);
+                secondaryMap.computeIfAbsent(model.getItemId(), id -> new ItemCounter()).addSum(preference - model.getPreference());
+            }
+            itemByMember.put(model.getItemId(), model.getPreference());
+        }
     }
     public Map<Long, Map<Long, ItemCounter>> getMatrix() {
         return matrix;
     }
 
-    public List<DataModel> getDataByMember(Long memberId) {
-        return dataByMember.getOrDefault(memberId, new ArrayList<>());
+    public Map<Long, Double> getDataByMember(Long id) {
+        return dataByMember.get(id);
     }
 }
--- backend/src/main/java/com/jujeol/drink/recommend/infrastructure/slope/Recommender.java
+++ backend/src/main/java/com/jujeol/drink/recommend/infrastructure/slope/Recommender.java
@@ -11,23 +11,23 @@ public class Recommender {
 
     public List<RecommendationResponse> recommend(DataMatrix dataMatrix, Long memberId, double minPreference) {
         final Map<Long, Map<Long, ItemCounter>> matrix = dataMatrix.getMatrix();
-        final List<DataModel> dataByMember = dataMatrix.getDataByMember(memberId);
+        final Map<Long, Double> dataByMember = dataMatrix.getDataByMember(memberId);
         final List<RecommendationResponse> recommendItems = new ArrayList<>();
 
         for (Entry<Long, Map<Long, ItemCounter>> matrixEntry : matrix.entrySet()) {
             final Long primaryItemId = matrixEntry.getKey();
             final Map<Long, ItemCounter> matrixValue = matrixEntry.getValue();
-            if(dataByMember.contains(new DataModel(memberId, primaryItemId, 0.0))) {
-                continue;
-            }
+            if(dataByMember.containsKey(primaryItemId)) continue;
 
             double sumValue = 0.0;
             long count = 0;
 
-            for (DataModel dataModel : dataByMember) {
-                final ItemCounter itemCounter = matrixValue.get(dataModel.getItemId());
+            for (Entry<Long, Double> itemPreference : dataByMember.entrySet()) {
+                final Long itemId = itemPreference.getKey();
+                final Double preference = itemPreference.getValue();
+                final ItemCounter itemCounter = matrixValue.get(itemId);
                 final double deviation = itemCounter.getDeviation();
-                sumValue += (dataModel.getPreference() + deviation) * itemCounter.getCount();
+                sumValue += (preference + deviation) * itemCounter.getCount();
                 count += itemCounter.getCount();
             }
 
