985765222e264144fceec2bf8968e7044dc068d6
bomin <bomin_93@naver.com>
2021-10-22 13:25:43 +0900

    fix: 선호도 없는 상태에서 추천 불가능한 버그 픽스 및 유레카 설정 삭제

--- backend/src/main/java/com/jujeol/JujeolApplication.java
+++ backend/src/main/java/com/jujeol/JujeolApplication.java
@@ -4,12 +4,10 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableEurekaClient
 public class JujeolApplication {
 
     @PostConstruct
