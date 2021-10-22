47fc64ee37913e6cf18a85894af823612345e3bd
bomin <bomin_93@naver.com>
2021-10-22 18:46:41 +0900

    fix: 선호도로 정렬해서 카테고리 검색하는 코드 수정

--- backend/src/main/java/com/jujeol/drink/drink/application/DrinkService.java
+++ backend/src/main/java/com/jujeol/drink/drink/application/DrinkService.java
@@ -44,7 +44,7 @@ public class DrinkService {
                     .map(drink -> DrinkDto.create(drink, preferenceService.showByMemberIdAndDrink(loginMember.getId(), drink)));
         }
 
-        return drinkRepository.findAllByCategory(category, pageable)
+        return drinkRepository.findAllByCategorySorted(category, pageable)
                 .map(drink -> DrinkDto.create(drink, preferenceService.showByMemberIdAndDrink(loginMember.getId(), drink)));
     }
 
