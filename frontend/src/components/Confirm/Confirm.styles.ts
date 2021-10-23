248e32c9d8a116a75de41ee8b63544c1e4056a05
sunhpark42 <sunhpark42@gmail.com>
2021-10-23 15:15:33 +0900

    refactor: 스타일 순서 정리

--- frontend/src/components/Confirm/Confirm.styles.ts
+++ frontend/src/components/Confirm/Confirm.styles.ts
@@ -3,15 +3,15 @@ import { COLOR } from 'src/constants';
 import Flex from 'src/styles/Flex';
 
 const Wrapper = styled.div<{ isOpened: boolean }>`
+  visibility: ${({ isOpened }) => (isOpened ? 'visible' : 'hidden')};
+  ${Flex({ justifyContent: 'center', alignItems: 'center' })}
   position: absolute;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
 
-  background-color: ${COLOR.BLACK + 'cc'};
-  visibility: ${({ isOpened }) => (isOpened ? 'visible' : 'hidden')};
-  ${Flex({ justifyContent: 'center', alignItems: 'center' })}
+  background-color: ${COLOR.BLACK}cc;
 `;
 
 const Content = styled.div`
@@ -19,10 +19,10 @@ const Content = styled.div`
   min-height: 25vh;
   padding: 0 2rem 2rem;
 
-  position: relative;
-  line-break: anywhere;
-
   ${Flex({ justifyContent: 'center', alignItems: 'center', flexDirection: 'column' })}
+  position: relative;
+
+  line-break: anywhere;
 
   p {
     font-size: 0.9rem;
@@ -30,13 +30,12 @@ const Content = styled.div`
 `;
 
 const ButtonWrapper = styled.div`
+  height: 2.5rem;
+
   position: absolute;
   left: 0;
   right: 0;
   bottom: 0;
-
-  width: 100%;
-  height: 2.5rem;
 
   border-bottom-left-radius: 1rem;
   border-bottom-right-radius: 1rem;
@@ -44,43 +43,54 @@ const ButtonWrapper = styled.div`
   button {
     width: 50%;
     height: 100%;
+
     border: inherit;
     background-color: transparent;
+
     color: ${COLOR.GRAY_700};
 
     :active {
       background-color: ${COLOR.PURPLE_100};
+
       color: ${COLOR.PURPLE_700};
     }
   }
 
   button:first-child {
     padding-left: 1rem;
+
     border-bottom-left-radius: inherit;
   }
 
   button:last-child {
     padding-right: 1rem;
+
     border-bottom-right-radius: inherit;
   }
 
   :before {
+    content: ' ';
+
     width: 100%;
     height: 0;
-    border-top: 1px solid ${COLOR.GRAY_200};
-    content: ' ';
+
     position: absolute;
     top: 0;
     left: 0;
+
+    border-top: 1px solid ${COLOR.GRAY_200};
   }
 
   :after {
     content: ' ';
+
+    width: 0;
+    height: 60%;
+
     position: absolute;
     bottom: 20%;
     left: 50%;
-    width: 0;
-    height: 60%;
+
     border-left: 1px solid ${COLOR.GRAY_200};
   }
 `;
