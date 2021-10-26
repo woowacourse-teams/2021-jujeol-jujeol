21dd87fb6d35cc05c874927f3fa9c904a900e1d5
sunhpark42 <sunhpark42@gmail.com>
2021-10-26 10:28:32 +0900

    refactor: login alert 위치 조정

--- admin/src/js/login.js
+++ admin/src/js/login.js
@@ -5,14 +5,15 @@ const $loginAlert = document.querySelector('#login-alert');
 
 $loginForm.addEventListener('submit', async (event) => {
   event?.preventDefault();
-  $loginAlert.textContent = '로그인 할 수 없습니다. 아이디, 비밀번호를 확인하세요.';
 
   // 로그인 요청 보내는 req
   try {
     // const response = await fetch(API_URL + '/login', {
     //   method: 'POST',
     // });
+    // history.push('/admin');
   } catch (error) {
     // 사용자 입력 에러
+    $loginAlert.textContent = '로그인 할 수 없습니다. 아이디, 비밀번호를 확인하세요.';
   }
 });
