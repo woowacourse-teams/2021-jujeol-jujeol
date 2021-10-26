f600225aa60cad6f992f9ca4ecd303d716cfeb94
sunhpark42 <sunhpark42@gmail.com>
2021-10-26 14:44:41 +0900

    feat: admin page loin 관련 api 연동 및 토큰 적용

--- admin/src/js/login.js
+++ admin/src/js/login.js
@@ -1,19 +1,39 @@
+const { ACCESS_TOKEN_KEY } = require('./constants');
+
 const $loginForm = document.querySelector('#login-form');
 const $loginAlert = document.querySelector('#login-alert');
 
-// const API_URL = process.env.API_URL;
+const API_URL = process.env.API_URL;
 
-$loginForm.addEventListener('submit', async (event) => {
+$loginForm?.addEventListener('submit', async (event) => {
   event?.preventDefault();
 
-  // 로그인 요청 보내는 req
+  const id = event.target['id'].value;
+  const password = event.target['password'].value;
+
   try {
-    // const response = await fetch(API_URL + '/login', {
-    //   method: 'POST',
-    // });
-    // history.push('/admin');
+    const response = await fetch(API_URL + '/admin/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id, password }),
+    });
+
+    if (!response.ok) {
+      throw new Error(await response.text());
+    }
+
+    const {
+      data: { accessToken },
+    } = await response.json();
+
+    localStorage.setItem(ACCESS_TOKEN_KEY, JSON.stringify(accessToken));
+
+    location.href = '/index.html';
   } catch (error) {
-    // 사용자 입력 에러
+    console.error(error);
+
     $loginAlert.textContent = '로그인 할 수 없습니다. 아이디, 비밀번호를 확인하세요.';
   }
 });
