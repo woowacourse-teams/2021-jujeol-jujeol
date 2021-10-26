ef109f5ccd9e35fdcb4cb31ffe79f138f7dcf8e8
bomin <bomin_93@naver.com>
2021-10-26 13:58:57 +0900

    feat: admin login 문서화

--- backend/src/main/java/com/jujeol/admin/ui/dto/AdminLoginRequest.java
+++ backend/src/main/java/com/jujeol/admin/ui/dto/AdminLoginRequest.java
@@ -1,10 +1,12 @@
 package com.jujeol.admin.ui.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
+@AllArgsConstructor
 public class AdminLoginRequest {
 
     private String id;
