df2933f5af872655b9edc1f4de37c9bcdc22ec35
Bomin Yoon <63535027+qhals321@users.noreply.github.com>
2021-11-16 06:55:28 +0900

    refactor: DataSource.getConnection 을 aop 형식으로 변경 (#599)

--- backend/src/test/java/com/jujeol/testdatabase/DatabaseConfig.java
+++ backend/src/test/java/com/jujeol/testdatabase/DatabaseConfig.java
@@ -1,7 +1,5 @@
 package com.jujeol.testdatabase;
 
-import javax.sql.DataSource;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,15 +9,5 @@ public class DatabaseConfig {
     @Bean
     public QueryCounter queryCounter() {
         return new QueryCounter();
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        final DataSource dataSource = DataSourceBuilder.create()
-                .driverClassName("org.h2.Driver")
-                .url("jdbc:h2:mem:test")
-                .username("SA")
-                .password("").build();
-        return new TestDataSource(dataSource, queryCounter());
     }
 }
